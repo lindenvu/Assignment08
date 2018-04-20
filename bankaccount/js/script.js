4aabdfd8c6f6890af119858c53a8f8b7b8ca24b3
lindenvu <github@lindenvu.com>
2018-04-19 21:32:43 -0700

    Update 1.1

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,15 +1,17 @@
 /*eslint-env browser*/
 
 var nameButton = window.document.getElementById("Name");
-var name;
-function getName() {
-    "use strict";
-    name = window.prompt("Enter your Name");
-}
-
-
-
-nameButton.addEventListener("click", getName());
-
 var output = window.document.getElementById("output");
-output.innerHTML = name;
+
+
+
+nameButton.addEventListener(
+    "click",
+    function () {
+        "use strict";
+        var name = window.prompt("Enter your Name");
+        output.innerHTML = name;
+    }
+);
+
+
